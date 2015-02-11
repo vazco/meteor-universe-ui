aab171bd37149102b0819ff9183d0c3fd2f2c98c
cristo-rabani <cristo.rabani@gmail.com>
2015-02-11 12:17:51 +0100

    fix

---
+++ ProvidingGlobals.js
@@ -0,0 +1,7 @@
+'use strict';
+/* global UniUI: true */
+/**
+ * UniUI is a global variable, available on client *
+ * @namespace UniUI
+ */
+UniUI = Object.create(null); //null creates object without prototype
